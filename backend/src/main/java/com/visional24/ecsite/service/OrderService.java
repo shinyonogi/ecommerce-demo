d2debf43fdd763be7c9c921e3793ded7e27d6e5d
Shintaro Onogi <shinyonogi@outlook.de>
2023-08-28 19:09:11 +0200

    Added Service Class for: Order

---
+++ backend/src/main/java/com/visional24/ecsite/service/OrderService.java
@@ -0,0 +1,33 @@
+package com.visional24.ecsite.service;
+
+import com.visional24.ecsite.model.entity.Order;
+import com.visional24.ecsite.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    private OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Order createOrder(Order order) {
+        return orderRepository.save(order);
+    }
+
+    public Optional<Order> getOrderById(Long id) {
+        return orderRepository.findById(id);
+    }
+
+    public List<Order> getAllOrder() {
+        return orderRepository.findAll();
+    }
+    
+}
