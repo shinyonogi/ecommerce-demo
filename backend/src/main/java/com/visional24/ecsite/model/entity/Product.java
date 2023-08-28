9a3296724442ef20a83df1648efd10518834f883
Shintaro Onogi <shinyonogi@outlook.de>
2023-08-28 18:07:42 +0200

    Becoming a Non-Commenter

--- backend/src/main/java/com/visional24/ecsite/model/entity/Product.java
+++ backend/src/main/java/com/visional24/ecsite/model/entity/Product.java
@@ -6,16 +6,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 
-//@Entityアノテーションは、このクラスがエンティティであり、JPAによってDBのテーブルとして管理されるということを示す。
 @Entity
 public class Product {
 
-    /*
-    @IDアノテーションは、エンティティのユニークなIDであることを示す。
-    @GeneratedValueアノテーションは、主キーの生成戦略を指定する。
-    Strategy = によって戦略を定義。(オプションは幾つか存在する)
-    GenerationType.IDENTITY 戦略は、データベースの自動インクリメントカラムを使用してIDを生成する。
-    */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -23,18 +16,14 @@ public class Product {
     private double price;
     private String description;
 
-    //JPAはデフォルトのコンストラクタを必要とする。
     public Product() {}
 
-    //カスタムコンストラクタを定義しても良い。
     public Product(long id, String name, double price, String description) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.description = description;
     }
-
-    //GetterとSetterを定義。
 
     public long getId() {
         return id;
--- backend/src/main/java/com/visional24/ecsite/service/ProductService.java
+++ backend/src/main/java/com/visional24/ecsite/service/ProductService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-//@Serviceアノテーションは、Springのコンテナにビジネスロジックを持つサービス層のコンポーネントとして登録される。
+
 @Service
 public class ProductService {
 
