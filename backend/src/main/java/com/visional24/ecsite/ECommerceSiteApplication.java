6a6225ebd4bfbafc89c426b5b6256d41a94ee3e6
Shintaro Onogi <shinyonogi@outlook.de>
2023-08-28 14:33:30 +0200

    Initialized Project

---
+++ backend/src/main/java/com/visional24/ecsite/ECommerceSiteApplication.java
@@ -0,0 +1,13 @@
+package com.visional24.ecsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceSiteApplication.class, args);
+	}
+
+}
