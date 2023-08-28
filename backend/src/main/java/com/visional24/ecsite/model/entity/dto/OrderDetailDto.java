25a132e659c911d48f94d420423c7725e3925e04
Shintaro Onogi <shinyonogi@outlook.de>
2023-08-29 01:01:08 +0200

    Added Dto Classes for: Order and Product

---
+++ backend/src/main/java/com/visional24/ecsite/model/entity/dto/OrderDetailDto.java
@@ -0,0 +1,26 @@
+package com.visional24.ecsite.model.entity.dto;
+
+public class OrderDetailDto {
+
+    private long productId;
+    private int quantity;
+    private double price;
+
+    public OrderDetailDto(int productId, int quantity, double price) {
+        this.productId = productId;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
---
+++ backend/src/main/java/com/visional24/ecsite/model/entity/dto/OrderDto.java
@@ -0,0 +1,47 @@
+package com.visional24.ecsite.model.entity.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class OrderDto {
+
+    private String customerName;
+    private String address;
+    private String email;
+    private LocalDate orderDate;
+    private double totalPrice;
+    private List<OrderDetailDto> orderDetails;
+
+    public OrderDto(String customerName, String address, String email, LocalDate orderDate, double totalPrice, List<OrderDetailDto> orderDetails) {
+        this.customerName = customerName;
+        this.address = address;
+        this.email = email;
+        this.orderDate = orderDate;
+        this.totalPrice = totalPrice;
+        this.orderDetails = orderDetails;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public List<OrderDetailDto> getOrderDetails() {
+        return orderDetails;
+    }
+}
