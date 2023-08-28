c0bc48dfff819c56c3f3ca0703d264536205de36
Shintaro Onogi <shinyonogi@outlook.de>
2023-08-28 18:02:30 +0200

    Created Controller Class for: Product

---
+++ backend/src/main/java/com/visional24/ecsite/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.visional24.ecsite.controller;
+
+import com.visional24.ecsite.model.entity.Product;
+import com.visional24.ecsite.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.findAllProducts();
+    }
+
+    @GetMapping("/{productId}")
+    public Optional<Product> getProductById(@PathVariable Long productId) {
+        return productService.findProductById(productId);
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+}
